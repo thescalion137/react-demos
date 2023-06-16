fff5b334331944790304758a018d0da05e1e0b4e
thescalion137 <thescalion137@gmail.com>
2023-06-16 15:10:42 +0530

    removed extra files

--- admin/backend/src/controllers/index.js
+++ admin/backend/src/controllers/index.js
@@ -1,11 +1,7 @@
 const UserController = require('./user.controller');
 const AuthController = require('./auth.controller');
-const PostController = require('./post.controller');
-const CommentController = require('./comment.controller');
 
 module.exports = {
   AuthController,
   UserController,
-  PostController,
-  CommentController,
 };
--- admin/backend/src/routes/index.js
+++ admin/backend/src/routes/index.js
@@ -1,9 +1,6 @@
 const indexRouter = require('express').Router();
 
 indexRouter.use('/auth', require('./auth.route'));
-// indexRouter.use('/user', require('./user.route'));
-// indexRouter.use('/post', require('./post.route'));
-// indexRouter.use('/comment', require('./comment.route'));
 indexRouter.use('/blog', require('./blog.route'));
 indexRouter.use('/event', require('./events.route'));
 
